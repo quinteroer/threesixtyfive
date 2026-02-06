b0b5a922a39326b2ba19bb0e623f19983f0919bb
quinteroer <quinteroer@vcu.edu>
2026-02-06 14:59:10 -0500

    Update calendar_data.js

--- calendar_data.js
+++ calendar_data.js
@@ -2,12 +2,12 @@ const loveData = {
     "day1": {
         "title": "Day 1",
         "message": "Happy Valentines, Belly. You're the love of my life and I am so full of happiness that I can be in your life. Being your boyfriend and your best friend is the highest privelidge.",
-        "song_embed": `<iframe allow=\`autoplay *; encrypted-media *; fullscreen *; clipboard-write\" frameborder=\"0\" height=\"175\" style=\"width:100%;max-width:660px;overflow:hidden;border-radius:10px;\" sandbox=\"allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation\" src=\"https://embed.music.apple.com/us/song/cant-help-falling-in-love/217635636\"></iframe>"
+        "song_embed": "<iframe allow=\`autoplay *; encrypted-media *; fullscreen *; clipboard-write\" frameborder=\"0\" height=\"175\" style=\"width:100%;max-width:660px;overflow:hidden;border-radius:10px;\" sandbox=\"allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation\" src=\"https://embed.music.apple.com/us/song/cant-help-falling-in-love/217635636\"></iframe>"
     },
     "day2": {
         "title": "Day 2",
         "message": "",
-        "song_embed": `<iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameborder="0" height="175" style="width:100%;max-width:660px;overflow:hidden;border-radius:10px;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/song/the-great-escape/276649797"></iframe>`
+        "song_embed": "<iframe allow=\`autoplay *; encrypted-media *; fullscreen *; clipboard-write\" frameborder=\"0\" height=\"175\" style=\"width:100%;max-width:660px;overflow:hidden;border-radius:10px;\" sandbox=\"allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation\" src=\"https://embed.music.apple.com/us/song/the-great-escape/276649797\"></iframe>"
     },
     "day3": {
         "title": "Day 3",
